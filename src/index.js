6ad018d9c6824c758d2d3bb8163664c917140f66
Harshal Shirote <harshalshirote2002@gmail.com>
2023-09-15 03:18:27 +0530

    delete old files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./styles.css";
-console.log("I am connected");
-
-const body = document.querySelector("body");
-const div = document.createElement("h1");
-div.textContent = "I am a level 1 heading!";
-body.appendChild(div);
