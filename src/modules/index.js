3d0d6d55c85b78bbc80aaa1b60425974baca6ebe
Harshal Shirote <harshalshirote2002@gmail.com>
2023-09-15 05:08:25 +0530

    Add footer Module

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,4 +1,6 @@
 import "../styles.css";
+import './fontawesome.js';
+import createFooter from './commonFooter.js';
 import createHeader from "./header.js";
 import updateDisplay from "./UI.js";
 
@@ -6,8 +8,9 @@ export default function updateUI() {
   const body = document.querySelector("body");
   const header = createHeader();
   const main = updateDisplay();
+  const footer = createFooter();
   body.textContent = '';
-  body.append(header, main);
+  body.append(header, main, footer);
 }
 
 updateUI();
